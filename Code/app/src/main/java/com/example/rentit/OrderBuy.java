2bfbcdb7e1cf30aeac71a04236fa053c1e938f78
Sumonta Saha Mridul <65423479+Jisan10667@users.noreply.github.com>
2023-01-27 16:03:25 +0600

    Complete Rent Take System including database read

---
+++ Code/app/src/main/java/com/example/rentit/OrderBuy.java
@@ -0,0 +1,14 @@
+package com.example.rentit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderBuy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_buy);
+    }
+}
