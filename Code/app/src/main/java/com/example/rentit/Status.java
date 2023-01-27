3235e6cab3012a5e7b534a8f456034e41395bf87
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-01-27 22:13:58 +0600

    User and Product Status Added with database

--- Code/app/src/main/java/com/example/rentit/Status.java
+++ Code/app/src/main/java/com/example/rentit/Status.java
@@ -1,14 +1,141 @@
 package com.example.rentit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class Status extends AppCompatActivity {
+
+    // recycler View
+    RecyclerView recyclerView;
+    // recyler  View
+
+    // database
+    FirebaseFirestore firebaseFirestore;
+    FirestoreRecyclerAdapter adapter;
+    // database
+
+    // data passing
+    String emails;
+    // data passing
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // hide the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        // hide the title bar
+
+        // login info passing
+        Intent intents = getIntent();
+        emails = intents.getStringExtra("emails");
+        // login info passing
+        
         setContentView(R.layout.activity_status);
+        
+        // Initializing variable
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        recyclerView = findViewById(R.id.showProduct);
+
+        //Query
+        // getting the data from database
+        Query query = firebaseFirestore.collection(emails); // collection name from database
+
+        FirestoreRecyclerOptions<UserTrack> options = new FirestoreRecyclerOptions.Builder<UserTrack>()
+                .setQuery(query, UserTrack.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<UserTrack, Status.StatusProductViewHolder>(options) {
+            @NonNull
+            @Override
+            public Status.StatusProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_list ,parent,false);
+                return new Status.StatusProductViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull Status.StatusProductViewHolder holder, int position, @NonNull UserTrack model) {
+
+                holder.name.setText(model.getName());
+                holder.description.setText(model.getDescription());
+                holder.rating.setText(model.getRating());
+                holder.head.setText(model.getHead());
+                holder.type.setText(model.getType());
+                holder.address.setText(model.getAddress());
+
+
+                String img ;
+                img = model.getImage();
+                Picasso.get().load(img).into(holder.image);
+
+            }
+        };
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private class StatusProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView head , name , description , rating , address,type;
+        ImageView image;
+        public StatusProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.user_name);
+            description = itemView.findViewById(R.id.user_des);
+            rating = itemView.findViewById(R.id.user_rating);
+            head = itemView.findViewById(R.id.user_head);
+            type = itemView.findViewById(R.id.user_type);
+            address = itemView.findViewById(R.id.user_price_address);
+            image = itemView.findViewById(R.id.user_image);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    public void onBackPressed() {
+
+        Intent intent = new Intent(Status.this, Menu.class);
+        // pass login data to menu
+        intent.putExtra("emails", emails);
+        // pass login data to menu
+        startActivity(intent); // go to sell screen
+
+        super.onBackPressed();
     }
 }
